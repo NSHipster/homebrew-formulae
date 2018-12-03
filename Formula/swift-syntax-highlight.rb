389d428afbffb2f682b0cad352550a460c04f9a9
Mattt <mattt@me.com>
2018-12-02 22:18:53 -0800

    Shorten formula description

--- Formula/swift-syntax-highlight.rb
+++ Formula/swift-syntax-highlight.rb
@@ -1,5 +1,5 @@
 class SwiftSyntaxHighlight < Formula
-  desc 'A syntax highlighter for Swift code that uses SwiftSyntax to generate Pygments-compatible HTML.'
+  desc 'A syntax highlighter for Swift code.'
   homepage 'https://github.com/NSHipster/SwiftSyntaxHighlighter'
   url 'https://github.com/NSHipster/SwiftSyntaxHighlighter.git', tag: '0.1.0', revision: '6c3e2dca81965f902694cff83d361986ad86f443'
   head 'https://github.com/NSHipster/SwiftSyntaxHighlighter.git', shallow: false
