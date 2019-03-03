6b01a8e7c606adc81242e8b16089e8a26bcb799c
Mattt <mattt@me.com>
2019-03-03 09:54:23 -0800

    Add formula for swift-format

---
+++ Formula/swift-format.rb
@@ -0,0 +1,16 @@
+class SwiftFormat < Formula
+  desc "A (proposed) official formatter for Swift"
+  homepage "https://github.com/NSHipster/swift-format"
+  url "https://github.com/NSHipster/swift-format.git", :branch => "format"
+  head "https://github.com/NSHipster/swift-format.git", :shallow => false
+
+  depends_on :xcode => ["10.0", :build]
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/swift-format"
+  end
+end
