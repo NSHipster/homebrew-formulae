9484a868a5e34724e9a4b949436d76062c075982
Mattt <mattt@nshipster.com>
2019-09-27 08:47:23 -0700

    Point xcprojectlint formula to official repository

--- Formula/xcprojectlint.rb
+++ Formula/xcprojectlint.rb
@@ -1,8 +1,8 @@
 class Xcprojectlint < Formula
   desc "A linter for Xcode project files"
   homepage "https://github.com/americanexpress/xcprojectlint"
-  url "https://github.com/nshipster/xcprojectlint.git", :tag => "0.1.0", :revision => "0c0e41d91fb6c45f57dce27d2fcc568596ece4ef"
-  head "https://github.com/nshipster/xcprojectlint.git", :shallow => false
+  url "https://github.com/americanexpress/xcprojectlint.git", :tag => "0.0.5"
+  head "https://github.com/americanexpress/xcprojectlint.git", :shallow => false
 
   depends_on :xcode => ["10.0", :build]
 
