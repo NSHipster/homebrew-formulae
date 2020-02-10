cf46d1d4b7c8092f7adee053fdc632e103ee1df8
조성빈 <37990858+pcr910303@users.noreply.github.com>
2020-02-10 12:30:02 +0900

    Update swift-format to apple's repo

--- Formula/swift-format.rb
+++ Formula/swift-format.rb
@@ -1,16 +1,20 @@
 class SwiftFormat < Formula
   desc "Formatting technology for Swift source code"
-  homepage "https://github.com/NSHipster/swift-format"
-  url "https://github.com/NSHipster/swift-format.git", :branch => "master"
-  head "https://github.com/NSHipster/swift-format.git", :shallow => false
+  homepage "https://github.com/apple/swift-format"
+  url "https://github.com/apple/swift-format.git", :branch => "swift-5.1-branch"
+  version "swift-5.1-branch"
+  head "https://github.com/apple/swift-format.git"
 
   depends_on :xcode => ["11.0", :build]
 
   def install
-    system "make", "install", "prefix=#{prefix}"
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    bin.install ".build/release/swift-format"
+    doc.install "Documentation/Configuration.md"
   end
 
   test do
-    system "#{bin}/swift-format"
+    (testpath/"test.swift").write " print(  \"Hello, World\"  ) ;"
+    assert_equal "print(\"Hello, World\")\n", shell_output("#{bin}/swift-format test.swift")
   end
 end
