1cb617a8f320e40d5ecb54e4b9a79e6daf9a8522
Mattt <mattt@me.com>
2020-11-09 10:46:52 -0800

    Update xcprojectlint formula

--- Formula/xcprojectlint.rb
+++ Formula/xcprojectlint.rb
@@ -1,16 +1,20 @@
-class Xcprojectlint < Formula
-  desc "A linter for Xcode project files"
-  homepage "https://github.com/americanexpress/xcprojectlint"
-  url "https://github.com/americanexpress/xcprojectlint.git", :tag => "0.0.5"
-  head "https://github.com/americanexpress/xcprojectlint.git", :shallow => false
+# typed: false
+# frozen_string_literal: true
 
-  depends_on :xcode => ["10.0", :build]
+# :nodoc:
+class Xcprojectlint < Formula
+  desc "Linter for Xcode project files"
+  homepage "https://github.com/americanexpress/xcprojectlint"
+  url "https://github.com/americanexpress/xcprojectlint.git", tag: "0.0.6", revision: "d9dad85847f5ee9b2143565a17d9066bb44b4b29"
+  version "0.0.6"
+
+  depends_on xcode: ["12.0", :build]
 
   def install
     system "make", "install", "prefix=#{prefix}"
   end
 
   test do
-    system "#{bin}/xcprojectlint" "--help"
+    system bin/"xcprojectlint", "--help"
   end
 end
