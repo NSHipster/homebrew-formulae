801c8b990e1b857e165cf38e9bcbd2cf2afab3a2
Mattt <mattt@me.com>
2020-11-09 10:48:36 -0800

    Update swift-format formula

--- Formula/swift-format.rb
+++ Formula/swift-format.rb
@@ -5,8 +5,8 @@
 class SwiftFormat < Formula
   desc "Formatting technology for Swift source code"
   homepage "https://github.com/NSHipster/swift-format"
-  version "0.50300.0"
   url "https://github.com/apple/swift-format.git", tag: "0.50300.0", revision: "12089179aa1668a2478b2b2111d98fa37f3531e3"
+  version "0.50300.0"
   head "https://github.com/apple/swift-format.git", shallow: false
 
   depends_on xcode: ["12.0", :build]
