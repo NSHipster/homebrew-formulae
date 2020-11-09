12a50801f25dc8d49e1813a49960337cdefeaeff
Mattt <mattt@me.com>
2020-11-09 10:39:54 -0800

    Update swift-syntax-highlight formula to 1.1.3

--- Formula/swift-syntax-highlight.rb
+++ Formula/swift-syntax-highlight.rb
@@ -1,16 +1,20 @@
+# typed: false
+# frozen_string_literal: true
+
+# :nodoc:
 class SwiftSyntaxHighlight < Formula
   desc "Syntax highlighter for Swift code"
   homepage "https://github.com/NSHipster/SwiftSyntaxHighlighter"
-  url "https://github.com/NSHipster/SwiftSyntaxHighlighter.git", :tag => "1.0.1", :revision => "a8a92e5db3c4a710036b47d24a5fc9b98901582a"
-  head "https://github.com/NSHipster/SwiftSyntaxHighlighter.git", :shallow => false
+  url "https://github.com/NSHipster/SwiftSyntaxHighlighter.git", tag: "1.1.3", revision: "b086ef5066b6f799f9c7d6d7fc1553581b55ef1e"
+  version "1.1.3"
 
-  depends_on :xcode => ["11.4", :build]
+  depends_on xcode: ["12.0", :build]
 
   def install
     system "make", "install", "prefix=#{prefix}"
   end
 
   test do
-    system "#{bin}/swift-highlight" "--help"
+    system bin/"swift-highlight", "--help"
   end
 end
