eb44433d88b5d3a521fad57cc186f762836f034b
Mattt <mattt@me.com>
2020-11-09 10:40:56 -0800

    Update uti formula

--- Formula/uti.rb
+++ Formula/uti.rb
@@ -1,8 +1,12 @@
+# typed: false
+# frozen_string_literal: true
+
+# :nodoc:
 class Uti < Formula
-  desc "A command-line utility that prints the path of the derived data directory for the current Xcode project"
+  desc "Prints the path of the derived data directory for the current Xcode project"
   homepage "https://github.com/NSHipster/uti"
-  url "https://github.com/NSHipster/uti.git", tag: "1.0.1"
-  head "https://github.com/NSHipster/uti.git", :shallow => false
+  url "https://github.com/NSHipster/uti.git", tag: "1.0.1", revision: "7f2cfafc410760354052542a3f112ea1fc577824"
+  version "1.0.1"
 
   depends_on :macos
 
