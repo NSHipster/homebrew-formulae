46419863056b0a14a485e3337cb8398b4154d320
Mattt <mattt@me.com>
2020-11-09 10:45:14 -0800

    Update xcderiveddata formula

--- Formula/xcderiveddata.rb
+++ Formula/xcderiveddata.rb
@@ -1,8 +1,12 @@
+# typed: false
+# frozen_string_literal: true
+
+# :nodoc:
 class Xcderiveddata < Formula
-  desc "A command-line utility that prints the path of the derived data directory for the current Xcode project"
+  desc "Prints the path of the derived data directory for the current Xcode project"
   homepage "https://github.com/NSHipster/xcderiveddata"
-  url "https://github.com/NSHipster/xcderiveddata.git", tag: "1.0.0"
-  head "https://github.com/NSHipster/xcderiveddata.git", :shallow => false
+  url "https://github.com/NSHipster/xcderiveddata.git", tag: "1.0.0", revision: "21a4bc3289f0cc04b8d4b835b2e00a1a2d144856"
+  version "1.0.0"
 
   depends_on :xcode
 
