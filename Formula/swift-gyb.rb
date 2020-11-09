cc75bc6d3c24b4cb023f283396d8ae78b3683f85
Mattt <mattt@me.com>
2020-11-09 10:36:45 -0800

    Update swift-gyb formula

--- Formula/swift-gyb.rb
+++ Formula/swift-gyb.rb
@@ -1,16 +1,22 @@
+# typed: false
+# frozen_string_literal: true
+
+# :nodoc:
 class SwiftGyb < Formula
   desc "Evaluates and runs Swift GYB scripts"
   homepage "https://github.com/NSHipster/swift-gyb"
-  url "https://github.com/NSHipster/swift-gyb.git", :tag => "0.1.0", :revision => "eebe89d6a76d870ea481f4523121fb2cf761a89e"
-  head "https://github.com/NSHipster/swift-gyb.git", :shallow => false
+  url "https://github.com/NSHipster/swift-gyb.git", tag: "0.1.0", revision: "eebe89d6a76d870ea481f4523121fb2cf761a89e"
+  version "0.1.0"
 
   depends_on "nshipster/formulae/gyb"
 
   def install
     system "swift", "build",
-      "--configuration", "release",
-      "-Xswiftc", "-static-stdlib",
-      "--disable-sandbox"
-    bin.install ".build/release/swift-gyb"
+           "--configuration", "release",
+           "--disable-sandbox",
+           "-Xswiftc", "-static-stdlib",
+           "--build-path", buildpath.to_s
+
+    bin.install buildpath/"release/swift-gyb" => "swift-gyb"
   end
 end
